19eb8304b5fbe9c633d799341874f50e768d40d6
Psilocke <rhysculley10@gmail.com>
2022-05-18 17:02:56 -0500

    starting from scratch

--- src/main/java/com/psilocke/crosscont/mixin/CrossbowMixin.java
+++ src/main/java/com/psilocke/crosscont/mixin/CrossbowMixin.java
@@ -11,25 +11,5 @@ import net.minecraft.world.level.Level;
 
 @Mixin(CrossbowItem.class)
 public abstract class CrossbowMixin {
-	/*
-	@Override
-	public InteractionResultHolder<ItemStack> use(Level p_40920_, Player p_40921_, InteractionHand p_40922_) {
-	      ItemStack itemstack = p_40921_.getItemInHand(p_40922_);
-	      if (isCharged(itemstack)) {
-	         performShooting(p_40920_, p_40921_, p_40922_, itemstack, getShootingPower(itemstack), 1.0F);
-	         setCharged(itemstack, false);
-	         return InteractionResultHolder.consume(itemstack);
-	      } else if (!p_40921_.getProjectile(itemstack).isEmpty()) {
-	         if (!isCharged(itemstack)) {
-	            this.startSoundPlayed = false;
-	            this.midLoadSoundPlayed = false;
-	            p_40921_.startUsingItem(p_40922_);
-	         }
-
-	         return InteractionResultHolder.consume(itemstack);
-	      } else {
-	         return InteractionResultHolder.fail(itemstack);
-	      }
-	   }
-	  */
+	
 }
